0d7f6fe968565c2fe0e86267597c46911b7f9a80
Nathan Chow <134994761+nchowcode@users.noreply.github.com>
2024-08-06 12:25:38 -0700

    Add feature to create grid

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 const container = document.querySelector('.container');
-const grid = document.createElement("div");
+const gridSize = 16;
+
+
+function createGrid() {
+    for (let i = 0; i < gridSize; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid-row");
+        for (let j = 0; j < gridSize; j++){
+            const gridColumn = document.createElement("div");
+            gridColumn.classList.add("grid-column");
+            grid.appendChild(gridColumn);
+        } 
+        container.appendChild(grid);
+    }
+}
+
+createGrid();
